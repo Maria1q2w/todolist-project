5cd1cd95bb8981c49336fc9b842a12aa0d3e5082
Rodion <redia1994@mail.ru>
2025-02-01 15:35:10 +0300

    лучше бы и не начинала

--- integration/addItemForm.test.js
+++ integration/addItemForm.test.js
@@ -1,6 +1,35 @@
+const puppeteer = require("puppeteer-core");
+
+// const { toMatchImageSnapshot } = require("jest-image-snapshot");
+
+// expect.extend({ toMatchImageSnapshot });
+
 describe("addItemForm", () => {
+    let browser;
+    let page;
+
+    beforeAll(async () => {
+        browser = await puppeteer.launch({ headless: true, executablePath: '/System/Volumes/Data/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' });
+        page = await browser.newPage();
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    jest.setTimeout(10000);
+
     it("base example, visually looks correct", async () => {
-        await page.goto("http://localhost:6006/iframe.html?path=/story/additemform-component--add-item-form-base-example");
-        expect(image).toMatchImageSnapshot();
+        await page.goto(
+            "http://localhost:6006/?path=/story/additemform-component--add-item-form-base-example"
+        );
+
+        const image = await page.screenshot();
+        expect(image).toMatchImageSnapshot({
+            failureThreshold: 0.01, // Допустимая погрешность (1% различий)
+            failureThresholdType: 'percent' // Тип порога: в процентах
+        });
+        
     });
 });
+
--- integration/jest.config.js
+++ integration/jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-    preset: "jest-puppeteer",
-    testRegex: "./*\\.test\\.js$",
-    setupFilesAfterEnv: ["./setupTests.js"]
+    launch: {
+        headless: true,
+        executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+    },
+    browserContext: "default"
 };
+
+// module.exports = {
+//     setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+//     testEnvironment: "node",
+//     browserContext: "default"
+// };
