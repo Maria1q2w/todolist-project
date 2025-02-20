4a503e6e2d4932c1b7ec85337ca90595a0e369ae
Rodion <redia1994@mail.ru>
2025-02-20 13:14:00 +0300

    Обновление проекта

--- src/state/api/todolists-api.ts
+++ src/state/api/todolists-api.ts
@@ -1,9 +1,12 @@
 import axios from "axios"
 
+const token = localStorage.getItem("token");
+
 const settings = {
     withCredentials: true,
     headers: {
-        "API-KEY": "8991b5d6-8bb8-4a7f-8b55-9a76b32fc425"
+        "API-KEY": "8991b5d6-8bb8-4a7f-8b55-9a76b32fc425",
+        Authorization: token ? `Bearer ${token}` : "",
     }
 }
 
